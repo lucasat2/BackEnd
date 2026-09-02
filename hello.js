c7581cdc85ca201dab20fd26ce77990a03344ebb
Lucas Ataide <lucaspaivacontato@gmail.com>
2026-09-02 10:34:32 -0300

    feat: implementado o dayjs para formatar datas.

--- hello.js
+++ hello.js
@@ -1,10 +1,13 @@
 var http = require('http')
+var dayjs = require('dayjs')
+var locaLizedFormat = require('dayjs/plugin/localizedFormat')
+dayjs.extend(locaLizedFormat)
 
 // Funcao que sera passada para o servidor
 
 function serverCallback(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' }); // Escreva no cabecalho um conteudo do tipo texto
-    res.end("Hello " + process.argv[2]);  // O conteudo será um Hello + a segunda posicao, que é alguma coisa que eu escrever ex: node hello.js Lucas
+    res.end("Hello " + process.argv[2] + " Hoje é dia " + dayjs().format('LLLL'));  // O conteudo será um Hello + a segunda posicao, que é alguma coisa que eu escrever ex: node hello.js Lucas
 }
 
 // Cria um servidor para ouvir na porta 8080
